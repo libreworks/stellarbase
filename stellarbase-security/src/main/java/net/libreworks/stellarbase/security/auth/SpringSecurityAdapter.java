c83d22a9836b0e0d8bf598c18aa600a90f5b3bc2
Jonathan Hawk <jonathan@libreworks.com>
2010-01-07 15:55:57 +0000

    Moving Security classes to their own module

---
+++ stellarbase-security/src/main/java/net/libreworks/stellarbase/security/auth/SpringSecurityAdapter.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2010 LibreWorks contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author Jonathan Hawk
+ */
+package net.libreworks.stellarbase.security.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * Principal adapter that delegates to the Spring Security context.
+ * 
+ * @author Jonathan Hawk
+ * @version $Id$
+ */
+public class SpringSecurityAdapter implements PrincipalAdapter
+{
+	/* (non-Javadoc)
+	 * @see net.libreworks.stellarbase.security.PrincipalAdapter#getPrincipal()
+	 */
+	public Authentication getPrincipal()
+	{
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+}
